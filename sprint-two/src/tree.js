e195da5a19cb1c6b3fad26211f2c6893b3418e5a
Hack Reactor Student <student@hackreactor.com>
2018-03-08 14:18:25 -0800

    Implement Tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,29 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree = _.extend(newTree, treeMethods);
+  newTree.children = []; 
+  
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var childTree = Tree(value);
+  this.children.push(childTree);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  for (var i = 0 ; i < this.children.length; i++){
+    if (this.children[i].contains(target)) {
+      return true;    
+    }
+  }
+  return false;
 };
 
 
