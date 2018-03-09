0673dc5fb1939bada4070ef3a01186fbc6c9174f
Hack Reactor Student <student@hackreactor.com>
2018-03-09 10:56:56 -0800

    Implement Hash table and set min requirements.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,54 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index) === undefined){
+    this._storage.set(index,[]);
+  }
+  var hashArray = this._storage.get(index);
+  for (var i = 0; i < hashArray.length; i++) {
+    if (hashArray[i].get(0) === k){
+      hashArray[i].set(1,v);
+      return;
+    }
+  }
+  var tuple = LimitedArray(2);
+  tuple.set(0,k);
+  tuple.set(1,v);
+  this._storage.get(index).push(tuple);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var hashArray = this._storage.get(index);
+  for (var i = 0; i < hashArray.length; i++) {
+    if (hashArray[i].get(0) === k){
+      return hashArray[i].get(1);
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var hashArray = this._storage.get(index);
+    for (var i = 0; i < hashArray.length; i++) {
+      if (hashArray[i].get(0) === k){
+        hashArray.splice(i,1);
+    }
+  }
+  
+  
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+  *Right now, the functions are more tending towards linear,
+  depending on the size of the data set (5 items: const, 1k: linear). Once 
+  the resizing is implemented, the function will be more likely to be const. 
+  This also depends on the reliability of the spread of the hash function as well
+
  */
 
 
